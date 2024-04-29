ce745f824f66f0ad5ac27f97f70566b29b3410ef
JdotHoff <jdotbird@gmail.com>
2024-04-29 08:23:57 -0400

    first push

---
+++ PracticeLoops/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+int dateOfBirth = 1979;
+int currentYear =2024;
+int age = currentYear - dateOfBirth;
+System.Console.WriteLine("My age is " + age);
+
+System.Console.WriteLine("Please enter your name.");
+string? nameOfUser = Console.ReadLine();
+if (nameOfUser == "Patrick") System.Console.WriteLine("Hello, Patrick");
+else System.Console.WriteLine("Hello Stranger");
+
+System.Console.WriteLine("What is 1 +1?");
+string? result = Console.ReadLine();
+int resultn = Int32.Parse(result);
+if (resultn == 2) System.Console.WriteLine("Well Done.");
+else System.Console.WriteLine("Sorry, Wrong Answer.");
+
+
+string word = "Hi";
+System.Console.WriteLine(word.Length);
+
+
+int size = 5; 
+for (int i=1; i<= size; i++)
+{
+     for (int j=size; j >= i; --j )
+     {
+        System.Console.Write("*");
+     }
+    
+    System.Console.WriteLine();
+}
+
+
+
